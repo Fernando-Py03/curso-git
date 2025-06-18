b9f26802957dcb353b5f89c97ae6ae3c8adc11fd
Fernando03 <esquiviafernando33@gmail.com>
2025-06-18 15:27:04 -0500

    Revert "se eliminaron algunas cosas inecesarias mensaje.py"
    This reverts commit 2a186f2f2cb99d2a71653e01993aed5949e65ab7.

--- mensaje.py
+++ mensaje.py
@@ -1,3 +1,8 @@
+fernando = "Hola friend"
 saludo = "Como estas"
-bienvenida = " Bienvenido, " + saludo
+bienvenida = fernando + " Bienvenido, " + saludo
 print("hola fernando")
+
+edad = 40-20
+print (" tu edad es" + edad)
+print ("hola")
