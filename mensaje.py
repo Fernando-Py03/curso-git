2a186f2f2cb99d2a71653e01993aed5949e65ab7
Fernando03 <esquiviafernando33@gmail.com>
2025-06-18 14:51:01 -0500

    se eliminaron algunas cosas inecesarias mensaje.py

--- mensaje.py
+++ mensaje.py
@@ -1,7 +1,2 @@
-fernando = "Hola friend"
 saludo = "Como estas"
-bienvenida = fernando + " Bienvenido, " + saludo
-
-edad = 40-20
-print (" tu edad es" + edad)
-print ("hola")
+bienvenida = " Bienvenido, " + saludo
